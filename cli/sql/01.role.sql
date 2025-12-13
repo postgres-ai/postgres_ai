a43416b1e018f9c0638de925e484ea415a1457c4
Nik Samokhvalov <nik@niks-mbp.lan>
2025-12-13 11:18:40 -0800

    docs(cli): clarify role SQL template
    - Add example ROLE_STMT expansions to cli/sql/01.role.sql

--- cli/sql/01.role.sql
+++ cli/sql/01.role.sql
@@ -1,4 +1,15 @@
 -- Role creation / password update (template-filled by cli/lib/init.ts)
+--
+-- Example expansions (for readability/review):
+--   create user "postgres_ai_mon" with password '...';
+--   alter user "postgres_ai_mon" with password '...';
+--   do $$ begin
+--     if not exists (select 1 from pg_catalog.pg_roles where rolname = 'postgres_ai_mon') then
+--       create user "postgres_ai_mon" with password '...';
+--     else
+--       alter user "postgres_ai_mon" with password '...';
+--     end if;
+--   end $$;
 {{ROLE_STMT}}
 
 
